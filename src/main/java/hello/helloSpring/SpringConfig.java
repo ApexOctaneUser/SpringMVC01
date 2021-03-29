2017c178950a8a6af44dd1e13f00d7c1763f2201
ApexOctaneUser <dzpro0327@naver.com>
2021-03-30 04:38:40 +0900

    3/30

--- src/main/java/hello/helloSpring/SpringConfig.java
+++ src/main/java/hello/helloSpring/SpringConfig.java
@@ -1,21 +1,32 @@
 package hello.helloSpring;
 
-import hello.helloSpring.repository.MemberRepository;
-import hello.helloSpring.repository.MemoryMemberRepository;
+import hello.helloSpring.repository.*;
 import hello.helloSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+
 }
